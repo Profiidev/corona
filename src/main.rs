d0f451d1a4717c4a6a856ffcbf2d54c55e27489b
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-08-11 13:18:40 +0200

    chore: some animation testing

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 use std::{cell::RefCell, rc::Rc};
 
-use corona::Corona;
+use corona::{Corona, widgets::WidgetHandle};
+use slint::ComponentHandle;
 use smithay_client_toolkit::shell::wlr_layer::Anchor;
 
 mod ui;
+
+#[derive(Default)]
+struct PanelState {
+  widget: Option<WidgetHandle>,
+  component: Option<slint::Weak<ui::panel::Panel>>,
+}
 
 fn main() {
   tracing_subscriber::fmt::init();
 
   let mut corona = Corona::init().expect("Failed to initialize Corona state");
   let handle = corona.handle();
-  let panel_handle = Rc::new(RefCell::new(None));
+  let panel = Rc::new(RefCell::new(PanelState::default()));
 
   for output in corona.outputs() {
     let handle = handle.clone();
     let wl_output = output.clone();
-    let panel_handle = panel_handle.clone();
+    let panel = panel.clone();
+
     corona
       .widget_builder()
       .anchor(Anchor::TOP | Anchor::LEFT | Anchor::RIGHT)
@@ -24,26 +32,50 @@ fn main() {
       .build(&output, move |b: &mut ui::bar::Bar| {
         b.on_clicked(move || {
           let wl_output = wl_output.clone();
-          let panel_handle = panel_handle.clone();
-          handle.defer(move |corona| {
-            if let Some(panel) = panel_handle.borrow_mut().take() {
-              corona.destroy_widget(panel);
+          let panel = panel.clone();
+          let handle = handle.clone();
+
+          handle.clone().defer(move |corona| {
+            let open = panel
+              .borrow()
+              .component
+              .as_ref()
+              .and_then(|component| component.upgrade());
+
+            if let Some(component) = open {
+              component.invoke_toggle_open();
               return;
             }
 
-            let panel = corona
+            let widget = corona
               .widget_builder()
               .width(200)
               .height(200)
               .anchor(Anchor::TOP | Anchor::LEFT)
-              .build(&wl_output, |p: &mut ui::panel::Panel| {
-                p.on_clicked(move || {
-                  println!("Panel clicked");
-                });
+              .build(&wl_output, {
+                let panel = panel.clone();
+                move |p: &mut ui::panel::Panel| {
+                  panel.borrow_mut().component = Some(p.as_weak());
+
+                  p.on_clicked(|| {
+                    println!("Panel clicked");
+                  });
+
+                  p.on_closed(move || {
+                    let panel = panel.clone();
+                    handle.defer(move |corona| {
+                      let mut panel = panel.borrow_mut();
+                      panel.component = None;
+                      if let Some(widget) = panel.widget.take() {
+                        corona.destroy_widget(widget);
+                      }
+                    });
+                  });
+                }
               })
               .expect("Failed to create panel widget");
 
-            *panel_handle.borrow_mut() = Some(panel);
+            panel.borrow_mut().widget = Some(widget);
           });
         });
       })
