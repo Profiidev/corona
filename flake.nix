f627ea223d9071ab8870a1b71b525197d2b84add
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-09-17 17:28:56 +0200

    feat: added nix package

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Corona";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://projects.cache.profidev.io"
+    ];
+
+    extra-trusted-public-keys = [
+      "profidev.cachix.org:tg4xEn64UMdvA5jJYT8omo/CQHk8+spLyeGT2YAku70="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        packages.default = pkgs.callPackage ./nix/package.nix { };
+      }
+    );
+}
---
+++ nix/package.nix
@@ -0,0 +1,72 @@
+{
+  lib,
+  rustPlatform,
+  fetchCrate,
+  applyPatches,
+  pkg-config,
+  fontconfig,
+  libxkbcommon,
+  libxcb,
+  pipewire,
+  wayland,
+  vulkan-loader,
+}:
+
+let
+  gpuiVersion = "0.3.4";
+
+  vendorCrate =
+    pname: hash:
+    applyPatches {
+      name = "${pname}-${gpuiVersion}-patched";
+      src = fetchCrate {
+        inherit pname hash;
+        version = gpuiVersion;
+      };
+      patches = lib.filesystem.listFilesRecursive (../patches + "/${pname}");
+      patchFlags = [ "-p3" ];
+    };
+
+  gpui-pre = vendorCrate "gpui-pre" "sha256-BRyR2XTJEBdeZZ9NYP1aByDgAat7G+0+sn59O5oMlh8=";
+  gpui-pre-linux = vendorCrate "gpui-pre-linux" "sha256-RB7lXu/zYlOPL5o/2655G0WFRflNRMPKN4bvTsljcZ4=";
+in
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "corona";
+  version = "0.1.0";
+
+  __structuredAttrs = true;
+
+  src = ./..;
+
+  cargoLock = {
+    lockFile = ../Cargo.lock;
+  };
+
+  nativeBuildInputs = [
+    pkg-config
+    rustPlatform.bindgenHook
+  ];
+
+  buildInputs = [
+    fontconfig
+    libxkbcommon
+    libxcb
+    pipewire
+  ];
+
+  postPatch = ''
+    mkdir -p vendor
+    cp -r --no-preserve=mode,ownership ${gpui-pre} vendor/gpui-pre
+    cp -r --no-preserve=mode,ownership ${gpui-pre-linux} vendor/gpui-pre-linux
+  '';
+
+  postFixup = ''
+    patchelf --add-rpath ${
+      lib.makeLibraryPath [
+        wayland
+        vulkan-loader
+      ]
+    } $out/bin/corona
+  '';
+})
